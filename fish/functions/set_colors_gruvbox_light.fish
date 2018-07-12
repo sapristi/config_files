a1661946cda8cfb9742dc295de4c809ba1bee7ba
sapristi <megabigfoutoir@gmail.com>
2018-07-12 12:28:49 +0200

    fish colors

---
+++ fish/functions/set_colors_gruvbox_light.fish
@@ -0,0 +1,12 @@
+
+function set_colors_gruvbox_light
+    set -xg cc_bg     fbf1c7
+    set -xg cc_red    cc241d
+    set -xg cc_green  98971a
+    set -xg cc_yellow d79921
+    set -xg cc_blue   458588
+    set -xg cc_purple b16286
+    set -xg cc_aqua   689d6a
+    set -xg cc_grey   7c6f64
+    set -xg cc_orange d65d0e
+end
