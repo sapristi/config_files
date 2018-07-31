9963b4fbba969caba6702aa074d11f7b38c3ecb6
sapristi <megabigfoutoir@gmail.com>
2018-07-31 22:55:59 +0200

    fish git prompt

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -2,10 +2,10 @@
 function fish_right_prompt -d "Prints right prompt"
 
     
-    # set -g  __fish_git_prompt_showcolorhints true
-    # set -g __fish_git_prompt_show_informative_status true
-    # echo (__fish_git_prompt)
+    set -g  __fish_git_prompt_showcolorhints true
+    set -g __fish_git_prompt_show_informative_status true
+    echo (__fish_git_prompt)
 
-    echo (__informative_git_prompt)
+    # echo (__informative_git_prompt)
 
 end
