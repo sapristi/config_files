dd124110eb743dd68756ba8b137ac610b0c79012
sapristi <megabigfoutoir@gmail.com>
2019-03-11 20:57:13 +0100

    terminator config

--- fish/configs/config-casual.fish
+++ fish/configs/config-casual.fish
@@ -1,3 +1,7 @@
 set -xg _colors_theme gruvbox_light
 set -xg PATH $HOME/.local/bin $PATH
+set -xg OPAMKEEPBUILDDIR true
+set -xg OCAMLPARAM "_,bin-annot=1"
+
+
 status --is-interactive; and source (pyenv init -|psub)
--- fish/functions/emacs-dev.fish
+++
@@ -1,3 +0,0 @@
-function emacs-dev
-	emacs -q --load /home/sapristi/Documents/config\ files/emacs/emacs-dev/init.el $argv
-end
